cf1dc4ae17965da5a1ff01d96194c37b76007786
Taiki <tyoshikawa1106@gmail.com>
2015-08-10 22:13:27 +0900

    commit

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,9 +2,51 @@ class StaticPagesController < ApplicationController
   def home
   end
 
-  def help
+  def starter
   end
 
-  def about
+  def theme
+  end
+
+  def grids
+  end
+
+  def jumbotron
+  end
+
+  def narrow_jumbotron
+  end
+
+  def navbar
+  end
+
+  def static_top_navbar
+  end
+
+  def fixed_navbar
+  end
+
+  def cover
+  end
+
+  def carousel
+  end
+
+  def blog
+  end
+
+  def dashboard
+  end
+
+  def sign_in_page
+  end
+
+  def justified_nav
+  end
+
+  def sticky_footer
+  end
+
+  def sticky_footer_with_navbar
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,21 @@
 BootstrapApp::Application.routes.draw do
   root  'static_pages#home'
-  match '/help', to: 'static_pages#help', via: 'get'
-  match '/about', to: 'static_pages#about', via: 'get'
+  match '/starter', to: 'static_pages#starter', via: 'get'
+  match '/theme', to: 'static_pages#theme', via: 'get'
+  match '/grids', to: 'static_pages#grids', via: 'get'
+  match '/jumbotron', to: 'static_pages#jumbotron', via: 'get'
+  match '/narrow_jumbotron', to: 'static_pages#narrow_jumbotron', via: 'get'
+  match '/navbar', to: 'static_pages#navbar', via: 'get'
+  match '/static_top_navbar', to: 'static_pages#static_top_navbar', via: 'get'
+  match '/fixed_navbar', to: 'static_pages#fixed_navbar', via: 'get'
+  match '/cover', to: 'static_pages#cover', via: 'get'
+  match '/carousel', to: 'static_pages#carousel', via: 'get'
+  match '/blog', to: 'static_pages#blog', via: 'get'
+  match '/dashboard', to: 'static_pages#dashboard', via: 'get'
+  match '/sign_in_page', to: 'static_pages#sign_in_page', via: 'get'
+  match '/justified_nav', to: 'static_pages#justified_nav', via: 'get'
+  match '/sticky_footer', to: 'static_pages#sticky_footer', via: 'get'
+  match '/sticky_footer_with_navbar', to: 'static_pages#sticky_footer_with_navbar', via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
